72884473c911f1543e398bf9a3fc897526d595a1
滕时 <shiteng@shitengdemacbook-air.local>
2015-12-19 14:53:32 +0800

    Add UIButton Ext

---
+++ SWDriverTestBingo/SWDriverTestBingo/SWUILib/UIButtonExt/UIButton+SWUIButtonExt.m
@@ -0,0 +1,33 @@
+//
+//  UIButton+SWUIButtonExt.m
+//  DriverTestShot
+//
+//  Created by EShi on 9/29/15.
+//  Copyright © 2015 Eren. All rights reserved.
+//
+
+#import "UIButton+SWUIButtonExt.h"
+
+@implementation UIButton (SWUIButtonExt)
+- (void)centerImageAndTitle:(float)spacing
+{
+    // get the size of the elements here for readability
+    CGSize imageSize = self.imageView.frame.size;
+    CGSize titleSize = self.titleLabel.frame.size;
+    
+    // get the height they will take up as a unit
+    CGFloat totalHeight = (imageSize.height + titleSize.height + spacing);
+    
+    // raise the image and push it right to center it
+    self.imageEdgeInsets = UIEdgeInsetsMake(- (totalHeight - imageSize.height), titleSize.width / 2, 0.0, 0.0);
+    
+    // lower the text and push it left to center it
+    self.titleEdgeInsets = UIEdgeInsetsMake(0.0, - imageSize.width, - (totalHeight - titleSize.height),0.0);
+}
+
+- (void)centerImageAndTitle
+{
+    const int DEFAULT_SPACING = 6.0f;
+    [self centerImageAndTitle:DEFAULT_SPACING];
+}
+@end
