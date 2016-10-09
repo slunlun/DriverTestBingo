fb511ad6c271a7c4c59a4f8784a457664b7855f3
Eren <Eren.Shi@nextlabs.com>
2016-10-09 12:30:02 +0800

    update map display codes

--- SWDriverTestBingo/SWDriverTestBingo/MainContentTabTwo/SWDriverTestApplyViewController.m
+++ SWDriverTestBingo/SWDriverTestBingo/MainContentTabTwo/SWDriverTestApplyViewController.m
@@ -10,11 +10,12 @@
 #import <BaiduMapAPI_Base/BMKBaseComponent.h>//引入base相关所有的头文件
 
 #import <BaiduMapAPI_Map/BMKMapComponent.h>//引入地图功能所有的头文件
-
+#import <BaiduMapAPI_Location/BMKLocationService.h>
 #import <BaiduMapAPI_Search/BMKSearchComponent.h>//引入检索功能所有的头文件
 
-@interface SWDriverTestApplyViewController ()<BMKMapViewDelegate>
+@interface SWDriverTestApplyViewController ()<BMKMapViewDelegate, BMKLocationServiceDelegate>
 @property(nonatomic, strong) BMKMapView* mapView;
+@property(nonatomic, strong) BMKLocationService *locService;
 @end
 
 @implementation SWDriverTestApplyViewController
@@ -23,6 +24,9 @@
     [super viewDidLoad];
     
     _mapView = [[BMKMapView alloc]initWithFrame:CGRectMake(0, 0, 320, 480)];
+    _locService = [[BMKLocationService alloc] init];
+    _locService.delegate = self;
+    _locService.desiredAccuracy = 5.0;
     self.view = _mapView;
 }
 
@@ -31,6 +35,15 @@
     [super viewWillAppear:animated];
     [_mapView viewWillAppear];
     _mapView.delegate = self; // 此处记得不用的时候需要置nil，否则影响内存的释放
+    [_locService startUserLocationService];
+    _mapView.userTrackingMode = BMKUserTrackingModeNone;//设置定位的状态
+      BMKLocationViewDisplayParam* displayParam = [[BMKLocationViewDisplayParam alloc] init];
+    displayParam.isRotateAngleValid = true;//跟随态旋转角度是否生效
+    displayParam.isAccuracyCircleShow = true;//精度圈是否显示
+    displayParam.locationViewOffsetX = 0;//定位偏移量（经度）
+    displayParam.locationViewOffsetY = 0;//定位偏移量（纬度）
+    [_mapView updateLocationViewWithParam:displayParam];
+    _mapView.showsUserLocation = YES;
 }
 -(void)viewWillDisappear:(BOOL)animated
 {
@@ -44,14 +57,49 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - /**
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
+/*在将要启动定位时，会调用此函数
 */
+- (void)willStartLocatingUser
+{
+}
+
+/**
+ *在停止定位后，会调用此函数
+ */
+- (void)didStopLocatingUser
+{
+}
+
+/**
+ *用户方向更新后，会调用此函数
+ *@param userLocation 新的用户位置
+ */
+- (void)didUpdateUserHeading:(BMKUserLocation *)userLocation
+{
+    [_mapView updateLocationData:userLocation];
+}
+
+/**
+ *用户位置更新后，会调用此函数
+ *@param userLocation 新的用户位置
+ */
+- (void)didUpdateBMKUserLocation:(BMKUserLocation *)userLocation
+{
+     _mapView.centerCoordinate = userLocation.location.coordinate;
+    [_mapView updateLocationData:userLocation];
+   // [_locService stopUserLocationService];
+
+}
+
+/**
+ *定位失败后，会调用此函数
+ *@param error 错误号
+ */
+- (void)didFailToLocateUserWithError:(NSError *)error
+{
+    
+}
 
 @end
